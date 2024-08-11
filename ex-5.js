915318aaa7118ce3b16e80ad9ecc6d0af665cb6a
April Twenty <atcahriya.nna@gmail.com>
2024-08-11 23:09:40 +0700

    Answer Ex 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let Discount = 25/100;
+let netPrice = totalPrice - (totalPrice * Discount);
+console.log(netPrice);
